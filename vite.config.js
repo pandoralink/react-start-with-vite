7f2d298651cf0119c8ac692d7fa9c09ff4a1f018
poplink <2219535001@qq.com>
2022-12-03 23:19:25 +0800

    feat: 基于 JSX 运行

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,5 @@ export default defineConfig({
   server: {
     port: 5173, // 默认端口是 5173
   },
+  plugins: [react()],
 });
